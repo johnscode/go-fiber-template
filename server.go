dd472f9d5d13cb7c8c6f1f96588f6d26310232bd
John F <john@johnscode.com>
2024-02-04 17:33:33 -0600

    add logger

--- server.go
+++ server.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/caarlos0/env/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog"
 	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 type config struct {
@@ -17,6 +23,7 @@ func main() {
 	if err := env.Parse(&cfg); err != nil {
 		log.Fatalf("problem parsing config: %+v", err)
 	}
+	logger := setupLogger(context.Background(), filepath.Join(cfg.LogDir, "server.log"))
 
 	server := fiber.New()
 
@@ -25,5 +32,34 @@ func main() {
 	})
 
 	addrStr := fmt.Sprintf(":%d", cfg.Port)
-	server.Listen(addrStr)
+	err := server.Listen(addrStr)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("starting app server")
+	}
+}
+
+func setupLogger(ctx context.Context, logFilePath string) *zerolog.Logger {
+	var outWriter = os.Stdout
+	//logFilePath := filepath.Join(dir,"server.log")
+	if logFilePath != "" && logFilePath != "stdout" {
+		file, err := os.OpenFile(logFilePath,
+			os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0664)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		outWriter = file
+	}
+	cout := zerolog.ConsoleWriter{Out: outWriter, TimeFormat: time.RFC822}
+	cout.FormatLevel = func(i interface{}) string {
+		return strings.ToUpper(fmt.Sprintf("| %-6s|", i))
+	}
+	// uncomment to remove timestamp from logs
+	//out.FormatTimestamp = func(i interface{}) string {
+	//	return ""
+	//}
+	baseLogger := zerolog.New(cout).With().Timestamp().Logger()
+	logCtx := baseLogger.WithContext(ctx)
+	l := zerolog.Ctx(logCtx)
+
+	return l
 }
